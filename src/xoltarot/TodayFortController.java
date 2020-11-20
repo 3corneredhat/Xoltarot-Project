2b1d42a670cfb59b0f63a6b8d3d95ada58cb98c6
3corneredhat <46908346+3corneredhat@users.noreply.github.com>
2020-11-19 20:29:09 -0600

    Help Section and GUI Adjustments
    I added a help button and help window. I had to make adjustments in the size of the primary (main window) stage in order for it to fit. Doing so, I had to adjust all the other windows.
    
    Additional changes:
    
    -I had a runtime error for there not being a CSS file so I added mine from Lab 7 as a placeholder.
    
    -I removed a duplicate main method.

--- src/xoltarot/TodayFortController.java
+++ src/xoltarot/TodayFortController.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
